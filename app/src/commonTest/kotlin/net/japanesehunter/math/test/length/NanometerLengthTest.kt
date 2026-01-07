ab24aa0bc541b8f5d06ca597e32ce3957b063761
Int16 <integer16bit@gmail.com>
2026-01-07 18:42:22 +0900

    remove redundancy

--- app/src/commonTest/kotlin/net/japanesehunter/math/test/length/NanometerLengthTest.kt
+++ app/src/commonTest/kotlin/net/japanesehunter/math/test/length/NanometerLengthTest.kt
@@ -3,37 +3,31 @@ package net.japanesehunter.math.test.length
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import net.japanesehunter.math.test.length.NanometerLength.Companion.meters
+import net.japanesehunter.math.test.length.NanometerLength.Companion.millimeters
 
 class NanometerLengthTest :
   FunSpec({
     test("Unit conversion equality is based on the physical amount") {
-      with(NanometerLength) {
-        1000L.millimeters shouldBe 1.0.meters
-      }
+      1000L.millimeters shouldBe 1.0.meters
     }
 
     test("Double DSL rejects NaN") {
-      with(NanometerLength) {
-        shouldThrow<IllegalArgumentException> {
-          Double.NaN.meters
-        }
+      shouldThrow<IllegalArgumentException> {
+        Double.NaN.meters
       }
     }
 
     test("Double DSL rejects infinity") {
-      with(NanometerLength) {
-        shouldThrow<IllegalArgumentException> {
-          Double.POSITIVE_INFINITY.meters
-        }
+      shouldThrow<IllegalArgumentException> {
+        Double.POSITIVE_INFINITY.meters
       }
     }
 
     test("Sign is preserved for the DSL") {
-      with(NanometerLength) {
-        (-1)
-          .meters
-          .toLong(meters) shouldBe -1L
-      }
+      (-1)
+        .meters
+        .toLong(meters) shouldBe -1L
     }
   })
 
